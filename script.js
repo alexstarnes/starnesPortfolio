0ef652f924f552ead3b9d5ce99a94da813a3694d
Alex Starnes <alexander.starnes@boeing.com>
2020-01-21 12:21:12 -0600

    added social Icons and added 1600 width

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+window.onbeforeunload = function () {
+  location.reload(true);
+}
+
 window.onload = function () {
 
 document.getElementById("splash").style.opacity = 1;
 var photoSectionID = document.getElementById("photoSection");
-var photSectDescID = document.getElementById("photoSectionDesc");  
+var photSectDescID = document.getElementById("photoSectionDesc"); 
     
 setTimeout(function() {
   document.getElementById("splash").style.display = "none";
@@ -15,18 +19,17 @@ var arrow = document.getElementById("arrow");
 var footer = document.getElementById("footer");
 
 
-
 window.onscroll = function() {myFunction()};
 
 
 function myFunction() {
     if ( window.pageYOffset > 150 ) {
         arrow.style.opacity = 0;
-//        footer.style.display = "flex";
         footer.style.opacity = 1;
     }
     else {
         arrow.style.opacity = 1;  
+        footer.style.opacity = 0;
     }
     
 }
