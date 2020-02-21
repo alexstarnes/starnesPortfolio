031a274c0baaf76d2c86ebb47ec301000ecfd6a3
Alexander Starnes <alexander.starnes@boeing.com>
2020-02-21 08:38:26 -0600

    synced internal and external and updated resume

--- script.js
+++ script.js
@@ -34,16 +34,27 @@ setTimeout(function() {
     
 }
 
-
 var arrow = document.getElementById("arrow");
 var social = document.getElementById("social");
 var topNavBar = document.getElementById("topNavBar");
+var hero = document.getElementById("hero");
+var popupBackground = document.getElementById("popupBackground");
+var popupOverlay = document.getElementById("popupOverlay");
+var popupIframe = document.getElementById("popupIframe");
 
 
 window.onscroll = function() {myFunction()};
 
 
 function myFunction() {
+    if ( window.pageYOffset > 400 ) {
+        hero.style.opacity = 0.4;
+    }
+    else {
+        hero.style.opacity =  1 - ((window.pageYOffset/400)*0.6) ;
+        
+    }
+    
     if ( window.pageYOffset > 170 ) {
         arrow.style.opacity = 0;
         social.style.opacity = 1;
@@ -57,4 +68,43 @@ function myFunction() {
         topNavBar.style.boxShadow = "none";
     }
     
+}
+
+var bodyElement = document.body;
+
+function openFilePopup() {
+    
+   var url = arguments[0];
+//    alert(url);
+    
+var windowWidth = window.innerWidth;
+    
+    if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
+        window.open(url, '_blank');
+    }
+    else if (windowWidth < 760) {
+        popupBackground.style.opacity = '1';
+        popupBackground.style.zIndex = '90';
+        popupOverlay.style.top = '0';
+        bodyElement.style.overflow = 'hidden';
+        popupIframe.src = url;
+        }
+    else {
+        
+        popupBackground.style.opacity = '1';
+        popupBackground.style.zIndex = '90';
+        popupOverlay.style.top = '10vh';
+        bodyElement.style.overflow = 'hidden';
+        popupIframe.src = url;
+        
+    }
+          
+}
+
+function closeFilePopup() {
+        popupBackground.style.opacity = '0';
+        popupBackground.style.zIndex = '-20';
+        popupOverlay.style.top = '105vh';
+        bodyElement.style.overflow = 'auto';
+        popupIframe.src = "";
 }
