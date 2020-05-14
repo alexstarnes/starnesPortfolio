ec710a35129cfa48ac5c6ae12fec63d400590f0f
Alexander Starnes <alexander.starnes@boeing.com>
2020-05-14 10:33:52 -0400

    Revert "reverting for now..."
    This reverts commit 8d7672ac51390238791b0f6d665bcf9c47b9af53.

---
+++ caseScript.js
@@ -0,0 +1,21 @@
+window.onload = function () {
+    
+    displayMessage = document.getElementById("displayMessage");
+    displayMessageShort = document.getElementById("displayMessageShort");
+    
+    setTimeout(function() {
+        
+        displayMessage.style.bottom = "-60px";
+        displayMessage.style.opacity = 0;
+        
+    }, 6000);
+    
+    setTimeout(function() {
+        
+        displayMessageShort.style.opacity = "1";
+        displayMessageShort.style.bottom = "20px";
+        displayMessage.style.display = "none";
+        
+    }, 7000);
+    
+}
